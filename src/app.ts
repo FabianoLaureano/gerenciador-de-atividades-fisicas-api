b046c1b016ee94b1d7f28444aaf96eb305fc93a4
FabianoLaureano <fabianoandrelaureano@gmail.com>
2026-02-24 18:39:59 -0300

    adiciona documentacão da api com swagger e scalar

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,78 @@
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
+  ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import { z } from "zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifyApiReference from "@scalar/fastify-api-reference";
 
 const app = Fastify({ logger: true });
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "API gerenciadora de treinos",
+      description: "API para gerenciar treinos e exercícios",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "LocalHost",
+        url: "http://localhost:3333",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    sources: [
+      {
+        title: "API gerenciadora de treinos",
+        slug: "api-gerenciadora-de-treinos",
+        url: "http://localhost:3333/swagger.json",
+      },
+    ],
+  },
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/swagger.json",
+  schema: {
+    hide: true,
+  },
+  handler: async () => {
+    return app.swagger();
+  },
+});
+
 app.get("/", async function handler() {
   return { message: "API is running 🚀" };
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/health",
+  schema: {
+    description: "Health check endpoint",
+    response: {
+      200: z.object({
+        status: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return { status: "API is running 🚀" };
+  },
 });
 
 export { app };
