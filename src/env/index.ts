92d90b9e2ee410574e4f1f8a6fc9fda869ff962e
FabianoLaureano <fabianoandrelaureano@gmail.com>
2026-02-24 22:55:18 -0300

    adiciona as configuracões do better auth

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
 });
 
 export const _env = envSchema.safeParse(process.env); // tenta validar para ver se tem as informações acima
